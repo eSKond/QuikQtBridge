a43b59260f48910cd62e95199f5feeb913350df6
eSKond <eskon@cn.ru>
2021-09-07 00:23:56 +0700

    added starter script and configuration examples

---
+++ lua/QuikQtBridgeStart.lua
@@ -0,0 +1,7 @@
+package.cpath = package.cpath..[[;c:\Work\QuikQtBridge\build-QuikQtBridge-qt_5_15_5_vs2019_static_win64-Debug\?.dll]]
+
+require "QuikQtBridge"
+
+-- function OnStop()
+-- 	return OnStopServer()
+-- end
